babb7439481d78d7a5ae3cca82f9792b264b94a4
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-06-12 16:48:23 +0300

    Added multiple basic thread projects

---
+++ Threading/RetrievingThread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace RetrievingThread
+{
+    class Program
+    {
+	    private static Object obj = new Object();
+        static void Main(string[] args)
+        {
+	        ThreadPool.QueueUserWorkItem(ShowThreadInformation);
+			var th1 = new Thread(ShowThreadInformation);
+			th1.Start();
+        }
+
+	    private static void ShowThreadInformation(Object state)
+	    {
+		    lock (obj)
+		    {
+			    var th = Thread.CurrentThread;
+			    Console.WriteLine("Managed thread #{0}: ", th.ManagedThreadId);
+			    Console.WriteLine("   Background thread: {0}", th.IsBackground);
+			    Console.WriteLine("   Thread pool thread: {0}", th.IsThreadPoolThread);
+			    Console.WriteLine("   Priority: {0}", th.Priority);
+			    Console.WriteLine("   Culture: {0}", th.CurrentCulture.Name);
+			    Console.WriteLine("   UI culture: {0}", th.CurrentUICulture.Name);
+			    Console.WriteLine();
+			}
+	    }
+    }
+}
