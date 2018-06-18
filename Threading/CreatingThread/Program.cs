1e7b25496537dda71f13870cd4255a26ac416254
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-06-18 14:49:07 +0300

    Creating threads that start instance and static methods

---
+++ Threading/CreatingThread/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace CreatingThread
+{
+	public class Program
+    {
+	    public static void Main()
+        {
+			var serverObject = new ServerClass();
+
+			var instanceCaller = new Thread(serverObject.InstanceMethod);
+
+			instanceCaller.Start();
+	        Console.WriteLine("The Main() thread calls this after "
+	                          + "starting the new InstanceCaller thread.");
+
+			var staticCaller = new Thread(ServerClass.StaticMethod);
+
+			//Start the thread
+			staticCaller.Start();
+
+	        Console.WriteLine("The Main() thread calls this after "
+	                          + "starting the new StaticCaller thread.");
+		}
+    }
+
+	public class ServerClass
+	{
+		// The method that will be called when the thread is started.
+		public void InstanceMethod()
+		{
+			Console.WriteLine(
+				"ServerClass.InstanceMethod is running on another thread.");
+
+			// Pause for a moment to provide a delay to make
+			// threads more apparent.
+			Thread.Sleep(3000);
+			Console.WriteLine(
+				"The instance method called by the worker thread has ended.");
+		}
+
+		public static void StaticMethod()
+		{
+			Console.WriteLine(
+				"ServerClass.StaticMethod is running on another thread.");
+
+			// Pause for a moment to provide a delay to make
+			// threads more apparent.
+			Thread.Sleep(5000);
+			Console.WriteLine(
+				"The static method called by the worker thread has ended.");
+		}
+	}
+}
